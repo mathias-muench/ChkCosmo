07a219b5bab18a3588520f1897f004294cfd2a24
Mathias Muench <mathias.muench@tngtech.com>
2016-03-13 13:34:55 -0400

    map all colors to up down draft

--- color_updown.c
+++ color_updown.c
@@ -1,68 +1,57 @@
-/*
-LodePNG Examples
-
-Copyright (c) 2005-2012 Lode Vandevenne
-
-This software is provided 'as-is', without any express or implied
-warranty. In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-    1. The origin of this software must not be misrepresented; you must not
-    claim that you wrote the original software. If you use this software
-    in a product, an acknowledgment in the product documentation would be
-    appreciated but is not required.
-
-    2. Altered source versions must be plainly marked as such, and must not be
-    misrepresented as being the original software.
-
-    3. This notice may not be removed or altered from any source
-    distribution.
-*/
-
-#include "lodepng.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-3 ways to decode a PNG from a file to RGBA pixel data (and 2 in-memory ways).
-*/
+#include "lodepng.h"
 
-/*
-Example 1
-Decode from disk to raw pixels with a single function call
-*/
-void decodeOneStep(const char* filename)
+void doit(const char* filename)
 {
   unsigned error;
-  unsigned char* image;
+  unsigned int *image;
   unsigned width, height;
   int x, y;
 
-  error = lodepng_decode24_file(&image, &width, &height, filename);
+  error = lodepng_decode32_file((unsigned char **)&image, &width, &height, filename);
   if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
 
   /*use image here*/
   for (y = 0; y < height; y++) {
-	  for (x = 0; x < width * 3; x += 3) {
-		  unsigned char *r, *g, *b;
-#if 0
-		  printf("%02x %02x %02x\n", image[y * width + x * 3], image[y * width + x * 3 + 1], image[y * width + x * 3 + 2]);
-#endif
-		  r = image + (y * width * 3 + x);
-		  g = image + (y * width * 3 + x + 1);
-		  b = image + (y * width * 3 + x + 2);
-		  if (!(*r == 193 && *g == 255 && *b == 243)) {
-			*r = *g = *b = 0;
-		  }
+	  for (x = 0; x < width; x++) {
+		unsigned *p = image + (y * width + x);
+		  if (*p == 0xFFFF00FF) {
+			*p = 0xfff0f0f0;
+		  } else if (*p == 0xFFC837FF) {
+			*p = 0xffe0e0e0;
+		  } else if (*p == 0xFF8D42FF) {
+			*p = 0xffd0d0d0;
+		  } else if (*p == 0xFF4040FF) {
+			*p = 0xffc0c0c0;
+		  } else if (*p == 0xFF5277FF) {
+			*p = 0xffb0b0b0;
+		  } else if (*p == 0xFF6BC1FF) {
+			*p = 0xffa0a0a0;
+		  } else if (*p == 0xFF6EEBFF) {
+			*p = 0xff909090;
+		  } else if (*p == 0xFFF3FFC1) {
+			*p = 0xff606060;
+		  } else if (*p == 0xFFF8DC75) {
+			*p = 0xff505050;
+		  } else if (*p == 0xFFFBC543) {
+			*p = 0xff404040;
+		  } else if (*p == 0xFFFEAA03) {
+			*p = 0xff303030;
+		  } else if (*p == 0xFFFF9B2B) {
+			*p = 0xff202020;
+		  } else if (*p == 0xFFFF8C59) {
+			*p = 0xff101010;
+		  } else if (*p == 0xFFFF8080) {
+			*p = 0xff000000;
+		} else {
+			*p = 0xff808080;
+		}
 	}
   }
 
-  error = lodepng_encode24_file("xx.png", image, width, height);
+  error = lodepng_encode32_file("xx.png", (unsigned char *)image, width, height);
   if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
 
   free(image);
@@ -72,7 +61,7 @@ int main(int argc, char *argv[])
 {
   const char* filename = argc > 1 ? argv[1] : "test.png";
 
-  decodeOneStep(filename);
+  doit(filename);
 
   return 0;
 }
