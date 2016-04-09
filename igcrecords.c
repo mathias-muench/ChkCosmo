d2772b32a0293cff95c89ab3e212d1d929de584c
Mathias Muench <mathias.muench@tngtech.com>
2016-04-09 13:28:10 -0400

    Calculate distance between fixes

--- igcrecords.c
+++ igcrecords.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "igcrecords.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 struct hms {
 	int hh;
 	int mm;
@@ -26,6 +30,18 @@ void b_record_to_fix(const char *record, struct fix *fix) {
 void fix_delta(struct fix *r, const struct fix *a, const struct fix *b) {
 	r->time = a->time - b->time;
 	r->alt = a->alt - b->alt;
+}
+
+double fix_distance(const struct fix *a, const struct fix *b) {
+	double d;
+	double lat1 = M_PI / 180.0 * a->lat;
+	double lat2 = M_PI / 180.0 * b->lat;
+	double lon1 = M_PI / 180.0 * a->lon;
+	double lon2 = M_PI / 180.0 * b->lon;
+
+	d=(40000.0/(360.0*60.0))*((180*60)/M_PI)*2*asin(sqrt((sin((lat1-lat2)/2))*(sin((lat1-lat2)/2)) + cos(lat1)*cos(lat2)*(sin((lon1-lon2)/2))*(sin((lon1-lon2)/2))));
+
+	return d;
 }
 
 #ifdef CU
@@ -59,12 +75,22 @@ int parse_lon() {
 	return fix->lon == 12 + 26360 / 60000.0;
 }
 
+int distance() {
+	struct fix fix1_s, *fix1 = &fix1_s;
+	struct fix fix2_s, *fix2 = &fix2_s;
+
+	b_record_to_fix("B0723224729253N01154547EA030000298700208", fix1);
+	b_record_to_fix("B0723244729236N01154545EA030050299800208", fix2);
+	return (int)(1000.0 * fix_distance(fix1, fix2)) == 31;
+}
+
 int main(char **v, int c)
 {
 	cu_test(parse_time);
 	cu_test(parse_alt);
 	cu_test(parse_lon);
 	cu_test(parse_lat);
+	cu_test(distance);
 	return(!cu_result());
 }
 #endif
