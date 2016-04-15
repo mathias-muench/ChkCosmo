1e3b0b16df0a496f963033581dcbd0b7d564229e
Mathias Muench <mathias.muench@tngtech.com>
2016-04-15 16:23:54 -0400

    Some warnings about anonymous struct, but regression ok

--- remap.c
+++ remap.c
@@ -122,6 +122,14 @@ static void free_images(unsigned *images[24][2])
 	}
 }
 
+double mean(kvec_t(struct fix) *b_fixes, size_t i, size_t interval) {
+	struct fix lf = kv_A(*b_fixes, i - 1);
+	struct fix bf = kv_A(*b_fixes, i);
+	struct fix df;
+	fix_delta(&df, &bf, &lf);
+	return df.alt / (df.time * 3600);
+}
+	
 void doit(const char *dirname, const char *date)
 {
 	int error;
@@ -134,7 +142,6 @@ void doit(const char *dirname, const char *date)
 
 
 	char line[80];
-	double le = 0.0;
 	double sxy = 0;
 	double sx = 0;
 	double sy = 0;
@@ -157,76 +164,67 @@ void doit(const char *dirname, const char *date)
 	for (i = 1; i < kv_size(b_fixes); i++) {
 		struct fix bf = kv_A(b_fixes, i);
 		struct fix lf = kv_A(b_fixes, i - 1);
-		double be = fix_e_kin(&bf, &lf);
-		if (le != 0.0) {
-			size_t alt;
-			if (bf.alt >= 4000) {
-				alt = 1;
-			} else if (bf.alt >= 3000) {
-				alt = 0;
-			} else {
-				alt = 2;
-			}
-			if (alt != 2) {
-				double dh;
-				double de;
-				double du;
-				struct fix df;
-				fix_delta(&df, &bf, &lf);
-				dh = df.alt / (df.time * 3600);
-				de = (be - le) / 9.81;
-#if 0
-				dh += de;
-#endif
-
-				int x, y;
-				x = lat_rad(bf.lat) * sin(lon_rad(bf.lon)) + X_LON10;
-				y = lat_rad(bf.lat) * cos(lon_rad(bf.lon)) + Y_LAT0;
-
-				unsigned int *p;
-				p = images[(int) bf.time][alt] + (y * width + x);
-				if (*p == 0xFFFF00FF) {
-					du = 4.0;
-				} else if (*p == 0xFFC837FF) {
-					du = 2.5;
-				} else if (*p == 0xFF6F42FF) {
-					du = 1.5;
-				} else if (*p == 0xFF4040FF) {
-					du = 1.0;
-				} else if (*p == 0xFF5277FF) {
-					du = 0.75;
-				} else if (*p == 0xFF6BC1FF) {
-					du = 0.5;
-				} else if (*p == 0xFF80FFFF) {
-					du = 0.25;
-				} else if (*p == 0xFFF3FFC1) {
-					du = -0.25;
-				} else if (*p == 0xFFF8DC75) {
-					du = -0.5;
-				} else if (*p == 0xFFFBC543) {
-					du = -0.75;
-				} else if (*p == 0xFFFEA803) {
-					du = -1.0;
-				} else if (*p == 0xFFFF9B2B) {
-					du = -1.5;
-				} else if (*p == 0xFFFF8C59) {
-					du = -2.5;
-				} else if (*p == 0xFFFF8080) {
-					du = -4.0;
-				} else {
-					du = 0.0;
-				}
-
-				printf("%6.2f %6.2f\n", dh, du);
-				sxy += dh * du;
-				sx += dh;
-				sy += du;
-				sxx += dh * dh;
-				syy += du * du;
-				nr++;
-			}
+		size_t alt;
+		if (bf.alt >= 4000) {
+			alt = 1;
+		} else if (bf.alt >= 3000) {
+			alt = 0;
+		} else {
+			alt = 2;
 		}
-		le = be;
+		if (alt != 2) {
+			double dh;
+			double de;
+			double du;
+
+			dh = mean(&b_fixes, i, 1);
+
+			int x, y;
+			x = lat_rad(bf.lat) * sin(lon_rad(bf.lon)) + X_LON10;
+			y = lat_rad(bf.lat) * cos(lon_rad(bf.lon)) + Y_LAT0;
+
+			unsigned int *p;
+			p = images[(int) bf.time][alt] + (y * width + x);
+			if (*p == 0xFFFF00FF) {
+				du = 4.0;
+			} else if (*p == 0xFFC837FF) {
+				du = 2.5;
+			} else if (*p == 0xFF6F42FF) {
+				du = 1.5;
+			} else if (*p == 0xFF4040FF) {
+				du = 1.0;
+			} else if (*p == 0xFF5277FF) {
+				du = 0.75;
+			} else if (*p == 0xFF6BC1FF) {
+				du = 0.5;
+			} else if (*p == 0xFF80FFFF) {
+				du = 0.25;
+			} else if (*p == 0xFFF3FFC1) {
+				du = -0.25;
+			} else if (*p == 0xFFF8DC75) {
+				du = -0.5;
+			} else if (*p == 0xFFFBC543) {
+				du = -0.75;
+			} else if (*p == 0xFFFEA803) {
+				du = -1.0;
+			} else if (*p == 0xFFFF9B2B) {
+				du = -1.5;
+			} else if (*p == 0xFFFF8C59) {
+				du = -2.5;
+			} else if (*p == 0xFFFF8080) {
+				du = -4.0;
+			} else {
+				du = 0.0;
+			}
+
+			printf("%6.2f %6.2f\n", dh, du);
+			sxy += dh * du;
+			sx += dh;
+			sy += du;
+			sxx += dh * dh;
+			syy += du * du;
+			nr++;
+		}
 	}
 	double r = (nr * sxy - sx * sy) / sqrt((nr * sxx - sx * sx) * (nr * syy - sy * sy));
 	printf("# %f %d %f\n", r, nr, pvalue(nr, r));
