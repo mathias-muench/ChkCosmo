7921fb4edb2ba4466f73d87b7249c108e3ee5dd5
Mathias Muench <mathias.muench@tngtech.com>
2016-04-15 16:00:40 -0400

    Use dynamic array, regression OK

--- remap.c
+++ remap.c
@@ -22,6 +22,7 @@
 #include <assert.h>
 
 #include "lodepng/lodepng.h"
+#include "kvec.h"
 #include "igcrecords.h"
 
 static double zip(double q, double i, double j, double b)
@@ -141,84 +142,93 @@ void doit(const char *dirname, const char *date)
 	double sxx = 0;
 	double syy = 0;
 	unsigned nr = 0;
+
+	kvec_t(struct fix *) b_fixes;
+	kv_init(b_fixes);
 	while (fgets(line, 80, stdin)) {
 		if (line[0] == 'B') {
-			struct fix bf;
-			b_record_to_fix(line, &bf);
-			if (lf.alt != 0.0) {
-				double be = fix_e_kin(&bf, &lf);
-				if (le != 0.0) {
-					size_t alt;
-					if (bf.alt >= 4000) {
-						alt = 1;
-					} else if (bf.alt >= 3000) {
-						alt = 0;
-					} else {
-						alt = 2;
-					}
-					if (alt != 2) {
-						double dh;
-						double de;
-						double du;
-						struct fix df;
-						fix_delta(&df, &bf, &lf);
-						dh = df.alt / (df.time * 3600);
-						de = (be - le) / 9.81;
+			struct fix *bf = malloc(sizeof (struct fix));
+			b_record_to_fix(line, bf);
+			kv_push(struct fix *, b_fixes, bf);
+		}
+	}
+
+    int i;
+	for (i = 0; i < kv_size(b_fixes); i++) {
+		struct fix *bf = kv_A(b_fixes, i);
+		if (lf.alt != 0.0) {
+			double be = fix_e_kin(bf, &lf);
+			if (le != 0.0) {
+				size_t alt;
+				if (bf->alt >= 4000) {
+					alt = 1;
+				} else if (bf->alt >= 3000) {
+					alt = 0;
+				} else {
+					alt = 2;
+				}
+				if (alt != 2) {
+					double dh;
+					double de;
+					double du;
+					struct fix df;
+					fix_delta(&df, bf, &lf);
+					dh = df.alt / (df.time * 3600);
+					de = (be - le) / 9.81;
 #if 0
-						dh += de;
+					dh += de;
 #endif
 
-						int x, y;
-						x = lat_rad(bf.lat) * sin(lon_rad(bf.lon)) + X_LON10;
-						y = lat_rad(bf.lat) * cos(lon_rad(bf.lon)) + Y_LAT0;
+					int x, y;
+					x = lat_rad(bf->lat) * sin(lon_rad(bf->lon)) + X_LON10;
+					y = lat_rad(bf->lat) * cos(lon_rad(bf->lon)) + Y_LAT0;
 
-						unsigned int *p;
-						p = images[(int) bf.time][alt] + (y * width + x);
-						if (*p == 0xFFFF00FF) {
-							du = 4.0;
-						} else if (*p == 0xFFC837FF) {
-							du = 2.5;
-						} else if (*p == 0xFF6F42FF) {
-							du = 1.5;
-						} else if (*p == 0xFF4040FF) {
-							du = 1.0;
-						} else if (*p == 0xFF5277FF) {
-							du = 0.75;
-						} else if (*p == 0xFF6BC1FF) {
-							du = 0.5;
-						} else if (*p == 0xFF80FFFF) {
-							du = 0.25;
-						} else if (*p == 0xFFF3FFC1) {
-							du = -0.25;
-						} else if (*p == 0xFFF8DC75) {
-							du = -0.5;
-						} else if (*p == 0xFFFBC543) {
-							du = -0.75;
-						} else if (*p == 0xFFFEA803) {
-							du = -1.0;
-						} else if (*p == 0xFFFF9B2B) {
-							du = -1.5;
-						} else if (*p == 0xFFFF8C59) {
-							du = -2.5;
-						} else if (*p == 0xFFFF8080) {
-							du = -4.0;
-						} else {
-							du = 0.0;
-						}
-
-						printf("%6.2f %6.2f\n", dh, du);
-						sxy += dh * du;
-						sx += dh;
-						sy += du;
-						sxx += dh * dh;
-						syy += du * du;
-						nr++;
+					unsigned int *p;
+					p = images[(int) bf->time][alt] + (y * width + x);
+					if (*p == 0xFFFF00FF) {
+						du = 4.0;
+					} else if (*p == 0xFFC837FF) {
+						du = 2.5;
+					} else if (*p == 0xFF6F42FF) {
+						du = 1.5;
+					} else if (*p == 0xFF4040FF) {
+						du = 1.0;
+					} else if (*p == 0xFF5277FF) {
+						du = 0.75;
+					} else if (*p == 0xFF6BC1FF) {
+						du = 0.5;
+					} else if (*p == 0xFF80FFFF) {
+						du = 0.25;
+					} else if (*p == 0xFFF3FFC1) {
+						du = -0.25;
+					} else if (*p == 0xFFF8DC75) {
+						du = -0.5;
+					} else if (*p == 0xFFFBC543) {
+						du = -0.75;
+					} else if (*p == 0xFFFEA803) {
+						du = -1.0;
+					} else if (*p == 0xFFFF9B2B) {
+						du = -1.5;
+					} else if (*p == 0xFFFF8C59) {
+						du = -2.5;
+					} else if (*p == 0xFFFF8080) {
+						du = -4.0;
+					} else {
+						du = 0.0;
 					}
+
+					printf("%6.2f %6.2f\n", dh, du);
+					sxy += dh * du;
+					sx += dh;
+					sy += du;
+					sxx += dh * dh;
+					syy += du * du;
+					nr++;
 				}
-				le = be;
 			}
-			lf = bf;
+			le = be;
 		}
+		lf = *bf;
 	}
 	double r = (nr * sxy - sx * sy) / sqrt((nr * sxx - sx * sx) * (nr * syy - sy * sy));
 	printf("# %f %d %f\n", r, nr, pvalue(nr, r));
